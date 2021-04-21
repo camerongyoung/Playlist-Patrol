79e74741dcac571aa39cc8051af67d4cbc7da0f5
camerongyoung <62030250+camerongyoung@users.noreply.github.com>
2021-04-20 20:51:25 -0400

    Update Playlist.cpp

--- Playlist.cpp
+++ Playlist.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include <map>
-#include <random>
-#include <ctime>
 #include <vector>
-#include <queue>
 #include <fstream>
 #include <string>
 #include <sstream>
 #include <ostream>
 #include <list>
+
+
 using namespace std;
 
+#include <chrono>
 #include <iostream>
 class Node
 {
 public:
     int score;
     string name;
-    list<string> names;
+    vector<string> names;
     Node* left;
     Node* right;
     int height;
@@ -27,11 +27,8 @@ Node* newNode(int score, string name)
     Node* currentNode = new Node();
     currentNode = currentNode;
     currentNode->score = score;
-    /*currentNode->name = name;*/
     currentNode->names.push_back(name);
     currentNode->height = 1;
-    
-    //cout << "successful" << endl;
 
     return currentNode;
 }
@@ -55,7 +52,6 @@ Node* rotateRight(Node* node)
 }
 Node* rotateLeft(Node* node)
 {
-    //your code here
     Node* newRoot = node->right;
     Node* swingNode = newRoot->left;
 
@@ -132,6 +128,42 @@ Node* insert(Node* root, int score, string name)
 
     return root;
 }
+void findTopTenAVL(Node* root, int& i, vector<pair<string, int>>& top10) {
+    if (root == NULL) {
+        return;
+    }
+    else {
+        if(root->right != NULL) {
+            findTopTenAVL(root->right, i, top10);
+        }
+        for(int j = 0; j < root->names.size(); j++) {
+            if (top10.size() == 11) {
+                return;
+            }
+            i++;
+            pair<string, int> pushedPair;
+            pushedPair.first = root->names[j];
+            pushedPair.second = root->score;
+            top10.push_back(pushedPair);
+        }
+        if(root->left != NULL) {
+            findTopTenAVL(root->left, i, top10);
+        }
+
+    }
+}
+
+void printTopTenAVL(vector<pair<string, int>>& top10) {
+    cout << "Song used as Algorithm Input: " << endl;
+    cout << "Name: " << top10[0].first << "; Score: " << top10[0].second << endl << endl;
+    cout << "Current Data Structure: AVL Tree" << endl;
+    cout << "----------------------------------------" << endl;
+    cout << "Top 10 Playlist: " << endl;
+    for(int j = 1; j < top10.size(); j++) {
+        cout << j << ". Name: " << top10[j].first << "; Score: " << top10[j].second << endl;
+    }
+    return;
+}
 
 struct MaxHeap
 {
@@ -205,9 +237,9 @@ class Songs
 public:
     Songs();
     Songs(
-        float& _acousticness, string& _artist, float& _dancability, int& _duration, float& _energy, int& _explct,
-        string& _id, float instrumentalness, int& _key, float& _liveness, float& _loudness, int& _mode, /*string& _name*/
-        int& _popularity, string release_date, float& _speechiness, float& _tempo, float& _valence, int& _year);
+            float& _acousticness, string& _artist, float& _dancability, int& _duration, float& _energy, int& _explct,
+            string& _id, float instrumentalness, int& _key, float& _liveness, float& _loudness, int& _mode, /*string& _name*/
+            int& _popularity, string release_date, float& _speechiness, float& _tempo, float& _valence, int& _year);
     float GetAcousticness();
     string GetArtist();
     float GetDancability();
@@ -273,9 +305,9 @@ Songs::Songs()
     year = 0;
 }
 Songs::Songs(
-    float& _acousticness, string& _artist, float& _dancability, int& _duration, float& _energy, int& _explct,
-    string& _id, float _instrumentalness, int& _key, float& _liveness, float& _loudness, int& _mode, /*string& _name*/
-    int& _popularity, string _release_date, float& _speechiness, float& _tempo, float& _valence, int& _year)
+        float& _acousticness, string& _artist, float& _dancability, int& _duration, float& _energy, int& _explct,
+        string& _id, float _instrumentalness, int& _key, float& _liveness, float& _loudness, int& _mode, /*string& _name*/
+        int& _popularity, string _release_date, float& _speechiness, float& _tempo, float& _valence, int& _year)
 {
     acousticness = _acousticness;
     artist = _artist;
@@ -474,8 +506,8 @@ void GetCSVData(string& filePath, map<string, Songs>& songs, map<string, int>& s
 
                 //cout << name << endl;
                 Songs s(
-                    acousticness, artist, dancability, duration, energy, explct, id, instrumentalness, key, liveness,
-                    loudness, mode, popularity, release_date, speechiness, tempo, valence, year);
+                        acousticness, artist, dancability, duration, energy, explct, id, instrumentalness, key, liveness,
+                        loudness, mode, popularity, release_date, speechiness, tempo, valence, year);
 
                 songs.emplace(name, s);
                 //cout << name << endl;
@@ -597,6 +629,15 @@ vector<pair<string, Songs>> TopSongs(MaxHeap& mh, map<string, Songs>& songs, int
     }
     return playlist;
 }
+void printTopTenHeap(vector<pair<string, Songs>> &playlist, map<string, int> &scores) {
+    cout << "Current Data Structure: Max Heap" << endl;
+    cout << "----------------------------------------" << endl;
+    cout << "Top 10 Playlist: " << endl;
+    for(int j = 0; j < playlist.size(); j++) {
+        cout << j+1 << ". Name: " << playlist[j].first << "; Score: " << scores[playlist[j].first] << endl;
+    }
+    return;
+}
 Node* insertScoresInTree(Node* root, map<string, int> scores)
 {
     map<string, int>::iterator it;
@@ -605,7 +646,7 @@ Node* insertScoresInTree(Node* root, map<string, int> scores)
     {
         //cout << "Size: " << tree.size(root) << endl;
         root = insert(root, it->second, it->first);
-        
+
     }
     return root;
 }
@@ -617,7 +658,8 @@ int main()
     string chosenSong;
     int listSize = 10;
     Node* root = NULL;
-    
+    int i = 0;
+
 
     map<string, Songs> songs;
     map<string, int> scores;
@@ -628,43 +670,59 @@ int main()
     cin >> number;
 
     switch (number) {
-    case 1:
-        chosenSong = "Keep A Song In Your Soul";
-        break;
-    case 2:
-        chosenSong = "I Put A Spell On You";
-        break;
-    case 3:
-        chosenSong = "#NOHOOK";
-        break;
-    case 4:
-        chosenSong = "Chicago Breakdown";
-        break;
-    case 5:
-        chosenSong = "90210 (feat. Kacy Hill)";
-        break;
-    case 6:
-        chosenSong = "Devil Town";
-        break;
-    case 7:
-        chosenSong = "Fire on the Mountain - Live at Soldier Field, Chicago, IL 7/3/2015";
-        break;
-    case 8:
-        chosenSong = "Trouble";
-        break;
-    case 9:
-        chosenSong = "Ease My Mind";
-        break;
-    case 10:
-        chosenSong = "Best Friend";
-        break;
+        case 1:
+            chosenSong = "Keep A Song In Your Soul";
+            break;
+        case 2:
+            chosenSong = "I Put A Spell On You";
+            break;
+        case 3:
+            chosenSong = "#NOHOOK";
+            break;
+        case 4:
+            chosenSong = "Chicago Breakdown";
+            break;
+        case 5:
+            chosenSong = "90210 (feat. Kacy Hill)";
+            break;
+        case 6:
+            chosenSong = "Devil Town";
+            break;
+        case 7:
+            chosenSong = "Fire on the Mountain - Live at Soldier Field, Chicago, IL 7/3/2015";
+            break;
+        case 8:
+            chosenSong = "Trouble";
+            break;
+        case 9:
+            chosenSong = "Ease My Mind";
+            break;
+        case 10:
+            chosenSong = "Best Friend";
+            break;
     }
 
     GetCSVData(filePath, songs, scores);
     applyScores(songs, scores, chosenSong);
-    //MakeHeap(scores, mh);
-    //vector<pair<string, Songs>> playlist = TopSongs(mh, songs, listSize);
-   
+    auto start = std::chrono::high_resolution_clock::now();
     root = insertScoresInTree(root, scores);
-    cout << "insert done: " << endl;
+    vector<pair<string, int>> top10AVL;
+    findTopTenAVL(root, i, top10AVL);
+    auto finish = std::chrono::high_resolution_clock::now();
+    auto duration = duration_cast<std::chrono::microseconds>(finish-start);
+    cout << "[AVL Tree] insert done: " << endl;
+    cout << "Task performed in " << duration.count() << " microseconds." << endl;
+    start = std::chrono::high_resolution_clock::now();
+    MakeHeap(scores, mh);
+    vector<pair<string, Songs>> playlist = TopSongs(mh, songs, listSize);
+    finish = std::chrono::high_resolution_clock::now();
+    duration = duration_cast<std::chrono::microseconds>(finish-start);
+    cout << "[Max Heap] insert done: " << endl;
+    cout << "Task performed in " << duration.count() << " microseconds." << endl;
+    cout << endl;
+
+    printTopTenAVL(top10AVL);
+    cout << endl;
+    printTopTenHeap(playlist, scores);
+
 }
